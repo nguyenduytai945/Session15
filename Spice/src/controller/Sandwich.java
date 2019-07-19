a6e5a668096bdaf326508f9a5dfa84e83a606cd4
Nguyễn duy Tài <nguyenduytai945 @ gmail>
2019-07-19 15:42:38 +0700

    add

---
+++ Spice/src/controller/Sandwich.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Sandwich {
+    @RequestMapping("/home")
+    public String loadIndex() {
+        return "index";
+    }
+    @GetMapping(value = "/save")
+    public String save(
+         @RequestParam("condiment") String[] condiment,ModelMap modelMap) {
+        modelMap.addAttribute("condiment", condiment);
+        return "save";
+    }
+}
---
+++ Spice/web/WEB-INF/views/save.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: Tran Anh Tuan
+  Date: 8/17/2018
+  Time: 4:28 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${condiment[0]}
+${condiment[1]}
+${condiment[2]}
+${condiment[3]}
+${condiment[4]}
+</body>
+</html>
